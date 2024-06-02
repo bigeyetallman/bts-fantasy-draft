0dc77569eed630a5a20ed8a1656a9428546876f8
bigeyetallman <bigeyetallman@gmail.com>
2024-06-02 14:39:53 +0900

    feat: 버전명 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 6
-        versionName = "1.02.02"
+        versionName = "1.02.00"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
